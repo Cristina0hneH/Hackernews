1d2f28aa0c997606dafb98f65aca065a0b57d930
Manuela Jimenez de Arechaga <mjdadesign@gmail.com>
2022-08-22 15:35:57 +0200

    mock server

--- README.md
+++
@@ -1,2 +0,0 @@
-# hackernews
-Hacker news project
---
+++ hackernews/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import hackernews from './data/hackernews.json'
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
